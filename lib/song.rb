832964b9390fc415e017a5c5a9a20f92cd58381c
David Ritchey <ritchey0713@gmail.com>
2018-12-09 15:16:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,77 +1,1 @@
-require 'pry'
-
-class Song 
-  
-  attr_accessor :name, :artist, :genre 
-  
-  @@count = 0 
-  @@genres = []
-  @@artists = []
-  
-  def initialize (song, artist, genre)
-    @@count +=1 
-    @@genres << genre 
-    @@artists << artist 
-    @name = song
-    @artist = artist
-    @genre = genre
-    
-  
-  end
-  
-  def self.count 
-    @@count
-  end
-  
-  def self.genres 
-    @@genres.uniq!
-  end
-  
-  def self.artists
-    @@artists.uniq!
-  
-  end 
-  
-  def self.genre_count
-   genre_list = {}
-    @@genres.map do |genres|
-    #binding.pry 
-      if genre_list[genres]
-         genre_list[genres] += 1
-      else
-         genre_list[genres] = 1
-      end
-    end
-  genre_list
-end
-
-
-def self.artist_count 
-  artist_list = {}
-  @@artists.map do |artists|
-    if artist_list[artists]
-      artist_list[artists] += 1 
-    else 
-      artist_list[artists] = 1 
-    end
-  end 
-  artist_list
-end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Song  attr_accessor :name, :artist, :genre  @@count = 0  @@artists = []  @@genres = []  def initialize(name, artist, genre)    @@count += 1    @@artists << artist    @@genres << genre    @name = name    @artist = artist    @genre = genre  end  def self.count    @@count  end  def self.artists    @@artists = @@artists.uniq  end  def self.genres    @@genres = @@genres.uniq  end  def self.genre_count    histo = {}    @@genres.each do |element|     if histo[element]        histo[element] += 1      else        histo[element] = 1      end     end    histoend
